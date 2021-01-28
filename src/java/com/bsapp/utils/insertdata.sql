b03727ab99c2b78054c24d44a1853cd777a56cf6
rossmcauliffe <rossmcauliffe@192.168.0.7>
2021-01-28 11:30:07 +0000

    Last Final Commit

---
+++ src/java/com/bsapp/utils/insertdata.sql
@@ -0,0 +1,39 @@
+INSERT INTO USERDATA(EMAIL,PASSWORD,FNAME,LNAME,USERTYPE)
+VALUES('admin@admin.com', 'mypassword', 'System', 'Administrator','ADMIN');
+
+INSERT INTO USERDATA(EMAIL,PASSWORD,FNAME,LNAME,USERTYPE)
+VALUES('user1@user.com', 'mypassword', 'Donald', 'User','GENUSER');
+
+INSERT INTO PRODUCTS(NAME,DESCRIPTION,PRICE,IMAGE_LOCATION)
+VALUES('Optimism','The Big Prelude to Dissapointment',5.67,'optimism.jpg');
+
+INSERT INTO PRODUCTS(NAME,DESCRIPTION,PRICE,IMAGE_LOCATION)
+VALUES('Lazy','Just because you have to doesnt mean you need to',5.67,'lazy.jpg');
+
+INSERT INTO PRODUCTS(NAME,DESCRIPTION,PRICE,IMAGE_LOCATION)
+VALUES('Unique','Just because you are unique doesnt make you useful',5.97,'unique.jpg');
+
+INSERT INTO PRODUCTS(NAME,DESCRIPTION,PRICE,IMAGE_LOCATION)
+VALUES('Nepotism','Its not easy to soar like an eagle unless your parents were eagles',5.67,'nepotism2.jpg');
+
+INSERT INTO PRODUCTS(NAME,DESCRIPTION,PRICE,IMAGE_LOCATION)
+VALUES('Overconfidence','This is going to end in disaster',5.67,'overconfidence.jpg');
+
+INSERT INTO PRODUCTS(NAME,DESCRIPTION,PRICE,IMAGE_LOCATION)
+VALUES('Potential','Not Everybody gets to be an astronaut',5.67,'potential.jpg');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
